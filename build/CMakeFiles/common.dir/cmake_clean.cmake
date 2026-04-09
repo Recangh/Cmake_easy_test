3a37a771f9ae6e4980fcd5e5239658ef2cdd2d13
Recangh <wjx060916240607@qq.com>
2026-04-09 20:42:09 +0800

    add include,src

---
+++ build/CMakeFiles/common.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/common.dir/src/pnc_map.cpp.obj"
+  "CMakeFiles/common.dir/src/pnc_map.cpp.obj.d"
+  "CMakeFiles/common.dir/src/process.cpp.obj"
+  "CMakeFiles/common.dir/src/process.cpp.obj.d"
+  "D:/Demo/VsCode(C++)/CMake_easy/Cmake_easy_test/bin/libcommon.dll"
+  "D:/Demo/VsCode(C++)/CMake_easy/Cmake_easy_test/bin/libcommon.dll.manifest"
+  "D:/Demo/VsCode(C++)/CMake_easy/Cmake_easy_test/bin/libcommon.pdb"
+  "libcommon.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/common.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/planning_main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/planning_main.dir/planning_main.cpp.obj"
+  "CMakeFiles/planning_main.dir/planning_main.cpp.obj.d"
+  "D:/Demo/VsCode(C++)/CMake_easy/Cmake_easy_test/bin/planning_main.exe"
+  "D:/Demo/VsCode(C++)/CMake_easy/Cmake_easy_test/bin/planning_main.exe.manifest"
+  "D:/Demo/VsCode(C++)/CMake_easy/Cmake_easy_test/bin/planning_main.pdb"
+  "libplanning_main.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/planning_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
